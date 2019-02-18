a27309756b6cffd9381ed8bdcae40cf188ae3569
Steven Christopher <36132914+StevenChristopher@users.noreply.github.com>
2019-02-18 07:41:50 -0800

    Add files via upload

---
+++ for_Loop.cs
@@ -0,0 +1,39 @@
+/* SOURCE:                
+   AUTHOR:   
+   FILENAME: for_Loop.cs
+   PURPOSE:  Demonstrate the basic functionality of the for loop control structure.
+             This is a very simplistic look at how the for loop is used to step through.
+   STUDENT:  Steven Christopher
+   DATE:     JAN 25 2019
+*/
+
+// STYLE MODIFICATIONS: None.
+// FUNCTIONAL MODIFICATIONS: None.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nDisplay of variable value after each loop iteration:\n");
+			
+            // for loops are used to iterate through process a known number of times.
+			// variables i, j, and k are normally used a lopp control variables.
+			// the for loop variable must be declared, initialized and incremented.
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i + " ");                
+            }
+
+            Console.Write("\n\n<Press Any Key to Exit>");
+            Console.ReadKey();
+        }
+    }
+}
