7ae289a39fa40205cc544652bdaf75f7f1e10223
Steven Christopher <36132914+StevenChristopher@users.noreply.github.com>
2019-02-18 07:57:20 -0800

    Update and rename Hello_While_Loop.cs to 3) Hello_While_Loop.cs

--- 3) Hello_While_Loop.cs
+++ 3) Hello_While_Loop.cs
@@ -1,3 +1,14 @@
+/* SOURCE:                
+   AUTHOR:   
+   FILENAME: Hello_While_Loop.cs
+   PURPOSE:  Demonstrate the while loop control structure and its' basic functionality.
+   STUDENT:  Steven Christopher
+   DATE:     JAN 25 2019
+*/
+
+// STYLE MODIFICATIONS: None.
+// FUNCTIONAL MODIFICATIONS: None.
+
 using System;
 
 namespace Hello_While_Loop
