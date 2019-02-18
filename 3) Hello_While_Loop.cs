bbdc4146abd90632a6cda89956d9113531a5652e
Steven Christopher <36132914+StevenChristopher@users.noreply.github.com>
2019-02-18 08:20:36 -0800

    Update 3) Hello_While_Loop.cs

--- 3) Hello_While_Loop.cs
+++ 3) Hello_While_Loop.cs
@@ -10,19 +10,25 @@
 // FUNCTIONAL MODIFICATIONS: None.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Hello_While_Loop
 {
     class Program
     {
-        // Initialization of the required Main() method for the program
         static void Main(string[] args)
         {
             
             Console.WriteLine("\nHello, C# World!");
             Console.Write("\n--Press <ENTER> to exit...");
 
-            // This while ensures the user has to press ENTER to exit the program
+            // This while loop ensures the user has to press ENTER to exit the program.
+            // So long as the while condition in the partentheses is met, the loop continues.
+            // In this case, the user will be prompted to press <ENTER> and the program
+            // won't exit until the <ENTER> key is selected.
             while (Console.ReadKey(true).Key != ConsoleKey.Enter) { }
 
         }
