b505f4e3d7f77a1a9ac31751bf4738ebb0e5d000
Steven Christopher <36132914+StevenChristopher@users.noreply.github.com>
2019-02-23 16:32:04 -0800

    Update 11) Do_While_Loop.cs

--- 11) Do_While_Loop.cs
+++ 11) Do_While_Loop.cs
@@ -1,12 +1,9 @@
-/* SOURCE:   dot net perls             
-   AUTHOR:   
+/* SOURCE:   https://www.dotnetperls.com/do         
+   AUTHOR:   UNKNOWN
    FILENAME: Input_and_Output_v2.cs
-   PURPOSE:  Demonstrate the StringBuilder class which provides many useful methods for
-             representing a mutable string of characters. StringBuilder seems to be much
-             more intuitive in certain cases compared to the Console class. Nonetheless,
-             both have their place.
+   PURPOSE:  Demonstrate the use of a do-while loop control structure.
    STUDENT:  Steven Christopher
-   DATE:     JAN 25 2019
+   DATE:     JAN 27 2019
 */
 
 // STYLE MODIFICATIONS: None.
@@ -18,22 +15,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Do_While_Loop
+namespace do_while_Loop
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Given the following sorted integer array
             int[] sortedArray = new int[] { 25, 38, 68, 88, 121, 146, 177, 220, 233 };
+
+            // Declaration of two additional integer variables.
             int sum = 0;
             int i = 0;
 
+            // This loop will add each index value with the next in the integer array.
             do
             {
                 sum += sortedArray[i];
                 i++;
             } while (i < 9);
 
+            // Display the sum of indices
             Console.Write("\n\nSum of Array Indices = ");
             System.Console.WriteLine(sum);
             Console.Write("\n\n<Press Any Key to Exit>");
