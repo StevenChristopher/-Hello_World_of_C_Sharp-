a4a33b9819881daf83cb14d27ecb2e37d788335e
Steven Christopher <36132914+StevenChristopher@users.noreply.github.com>
2019-02-24 15:04:23 -0800

    Update 6) Random_Number_Generator.cs

--- 6) Random_Number_Generator.cs
+++ 6) Random_Number_Generator.cs
@@ -1,16 +1,22 @@
-/* SOURCE:                
-   AUTHOR:   
-   FILENAME: Input_and_Output_v2.cs
-   PURPOSE:  Demonstrate the StringBuilder class which provides many useful methods for
-             representing a mutable string of characters. StringBuilder seems to be much
-             more intuitive in certain cases compared to the Console class. Nonetheless,
-             both have their place.
+/* SOURCE:   https://docs.microsoft.com/en-us/dotnet/api/system.random?view=netframework-4.7.2             
+   AUTHOR:   MS
+   FILENAME: Random_Number_Generator.cs
+   PURPOSE:  Demonstrate a program designed to randomly generate a number using
+             the Random() method. It will do this three times to show that the number
+             range is between 1 and 10.
    STUDENT:  Steven Christopher
-   DATE:     JAN 25 2019
+   DATE:     JAN 28 2019
 */
 
-// STYLE MODIFICATIONS: None.
-// FUNCTIONAL MODIFICATIONS: None.
+// STYLE MODIFICATIONS: 
+// o Console display message is unique.
+// o Exit prompt message.
+// o I chose 1 through 10 as the range so that I can
+//   demonstrate the mathematical logic in previous Input_and_Output.cs program.
+
+// FUNCTIONAL MODIFICATIONS:
+// o Iterated through only 3 times.
+// o Exit prompt and program exit.
 
 using System;
 using System.Collections.Generic;
@@ -22,18 +28,19 @@ namespace Random_Number_Generator
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Random num = new Random();
 
-            byte[] numArray = new byte[10];
-            num.NextBytes(numArray);
+            Console.WriteLine("\nRandom Number Between 1 and 10:\n");
 
-            Console.WriteLine("\nTen random byte-sized numbers:");
-
-            foreach (byte byteValue in numArray)
-                Console.Write("\n{0, 10}\n", byteValue);
-
+            // Iterate through and display three different random numbers.
+            // Numbers should range between 1 and 10.
+            for (int ii = 0; ii <= 2; ii++)
+            {
+                Console.Write("{0, 8:N0} ", num.Next(1, 11));
+            }
+            Console.WriteLine();
             Console.Write("\n<Press Any Key to Exit>");
             Console.ReadLine();
         }
