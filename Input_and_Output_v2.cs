56cb5555b65ec36b525df5b669c2b2cd341c0254
Steven Christopher <36132914+StevenChristopher@users.noreply.github.com>
2019-03-06 15:03:27 -0800

    Update and rename 4) Input_and_Output_v2.cs to Input_and_Output_v2.cs

--- Input_and_Output_v2.cs
+++ Input_and_Output_v2.cs
@@ -1,19 +1,20 @@
 /* SOURCE:   https://docs.microsoft.com/en-us/dotnet/api/system.text.stringbuilder?view=netframework-4.7.2             
-   AUTHOR:   UNKNOWN
-   FILENAME: Input_and_Output_v2.cs
-   PURPOSE:  Demonstrate the StringBuilder class which provides many useful methods for
+ * AUTHOR:   
+ * FILENAME: Input_and_Output_v2.cs
+ * PURPOSE:  Demonstrate the StringBuilder class which provides many useful methods for
              representing a mutable string of characters. StringBuilder seems to be much
              more intuitive in certain cases compared to the Console class. Nonetheless,
              both have their place.
-   STUDENT:  Steven Christopher
-   DATE:     JAN 28 2019
+ * STUDENT:  Steven Christopher
+ * DATE:     JAN 28 2019
 */
 
 // STYLE MODIFICATIONS: 
-// o Used a section from previous Input_and_Output.cs program.
+// 1) Used a section from previous Input_and_Output.cs program as the output to
+//    be displayed.
 
 // FUNCTIONAL MODIFICATIONS:
-// o Prompt to exit program.
+// 1) Prompt to exit program.
 
 using System;
 using System.Collections.Generic;
