981598e02de2cde1645fab5713c029dcf7efc0a1
Steven Christopher <36132914+StevenChristopher@users.noreply.github.com>
2019-03-06 15:50:06 -0800

    Update and rename 8) Reverse_Word_Order.cs to Reverse_Word_Order.cs

--- Reverse_Word_Order.cs
+++ Reverse_Word_Order.cs
@@ -1,18 +1,18 @@
 /* SOURCE:   https://www.c-sharpcorner.com/UploadFile/c63ec5/reverse-string-in-C-Sharp/             
-   AUTHOR:   Sharad Gupta
-   FILENAME: Reverse_Word_Order.cs
-   PURPOSE:  Demonstration of the Array.Reverse() method. The user is
+ * AUTHOR:   Sharad Gupta
+ * FILENAME: Reverse_Word_Order.cs
+ * PURPOSE:  Demonstration of the Array.Reverse() method. The user is
              prompted to enter a sentence. The sentence is then reversed
              and printed.
-   STUDENT:  Steven Christopher
-   DATE:     JAN 31 2019
+ * STUDENT:  Steven Christopher
+ * DATE:     FEB 06 2019
 */
 
 // STYLE MODIFICATIONS:
-// o Added different colors to the console display text.
-// o Unique dialoge added.
-// o Changed the names of variables for better readablity.
-// o Put additional spacing between console messages.
+// 1) Added different colors to the console display text.
+// 2) Unique dialoge added.
+// 3) Changed the names of variables for better readablity.
+// 4) Put additional spacing between console messages.
 
 // FUNCTIONAL MODIFICATION: None.
 
@@ -32,7 +32,8 @@ namespace Reverse_Word_Order
             Console.WriteLine("\nEnter a sentence:\n");
             Console.ForegroundColor = ConsoleColor.Yellow;
             string s1 = Console.ReadLine();
-
+           
+            // Split() and Reverse() methods are the real players in this proram.
             string[] words = s1.Split(' ');
             Array.Reverse(words);
 
