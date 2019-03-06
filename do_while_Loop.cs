3618e7f131e5c70b4e8aad162f6c24609f3f379a
Steven Christopher <36132914+StevenChristopher@users.noreply.github.com>
2019-03-06 11:32:55 -0800

    Update and rename 2c) do_while_Loop.cs to do_while_Loop.cs

--- do_while_Loop.cs
+++ do_while_Loop.cs
@@ -1,19 +1,19 @@
 /* SOURCE:   https://www.dotnetperls.com/do         
-   AUTHOR:   UNKNOWN
-   FILENAME: do_while_Loop.cs
-   PURPOSE:  Demonstrate the use of a do-while loop control structure.
-   STUDENT:  Steven Christopher
-   DATE:     JAN 27 2019
+ * AUTHOR:   UNKNOWN
+ * FILENAME: do_while_Loop.cs
+ * PURPOSE:  Demonstrate the use of a do-while loop control structure.
+ * STUDENT:  Steven Christopher
+ * DATE:     JAN 27 2019
 */
 
 // STYLE MODIFICATIONS:
-// o Added console display description of what the results represent.
-// o Used the array from the Array_Sort.cs program.
-// o Different array name.
+// 1) Added console display description of what the results represent.
+// 2)  Used the array from the Array_Sort.cs program.
+// 3) Different array name.
 
 // FUNCTIONAL MODIFICATIONS:
-// o Used a different number of indices.
-// o Added prompt for program exit.
+// 1) Used a different number of indices.
+// 2) Added prompt for program exit.
 
 using System;
 using System.Collections.Generic;
@@ -27,7 +27,7 @@ namespace do_while_Loop
     {
         static void Main(string[] args)
         {
-            // Given the following sorted integer array
+            // Given the following sorted integer array.
             int[] sortedArray = new int[] { 25, 38, 68, 88, 121, 146, 177, 220, 233 };
 
             // Declaration of two additional integer variables.
